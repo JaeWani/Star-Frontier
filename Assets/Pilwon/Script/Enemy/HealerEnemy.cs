48f132585e94bc24540454dc6923ed77e8a13f37
Chosungwoo123 <ejyoo12345@naver.com>
2023-11-02 00:06:19 +0900

    Add a Light in InGame Scene.

--- Assets/Pilwon/Script/Enemy/HealerEnemy.cs
+++ Assets/Pilwon/Script/Enemy/HealerEnemy.cs
@@ -52,8 +52,11 @@ public class HealerEnemy : EnemyBase
         {
             foreach (var target in targets)
             {
-                // 적의 체력을 20% 회복시킨다.
-                target.GetComponent<EnemyBase>().HealEnemy(0.2f);
+                if (target.gameObject.activeSelf)
+                {
+                    // 적의 체력을 20% 회복시킨다.
+                    target.GetComponent<EnemyBase>().HealEnemy(0.2f);
+                }
             }
         }
     }
