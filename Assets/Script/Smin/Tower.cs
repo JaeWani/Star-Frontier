361c1779228fa7473bc1c2e2e169908e42efd93d
Chosungwoo123 <ejyoo12345@naver.com>
2023-11-02 00:38:22 +0900

    Make a Tower HealthBar.

--- Assets/Script/Smin/Tower.cs
+++ Assets/Script/Smin/Tower.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,26 @@ using UnityEngine.UI;
 
 public class Tower : MonoBehaviour
 {
-    [SerializeField] int hp;
+    [SerializeField] float hp;
     [SerializeField] Image damage_ui;
+    [SerializeField] private Image healthBarImage;
+
+    private float maxHp;
+
+    private void Start()
+    {
+        maxHp = hp;
+    }
+
+    private void Update()
+    {
+        HPBarUpdate();
+    }
+    
+    private void HPBarUpdate()
+    {
+        healthBarImage.fillAmount = Mathf.Lerp(healthBarImage.fillAmount, hp / maxHp, Time.deltaTime * 12f );
+    }
 
     public void Damage(){
         hp--;
